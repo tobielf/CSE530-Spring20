99e7b948fac2156c21f25b4aa86d536f0733b5aa
Xiangyu Guo <tobielf@gmail.com>
2020-03-16 00:59:50 -0700

    Update hcsr_device.c
    Refined my implementation on platform device part. This should not be hardcoded, since the user will specify more than two devices, and according to the physical board layout, maximum devices could be 10. So I changed it to dynamic allocation.

--- HW2/hcsr_device.c
+++ HW2/hcsr_device.c
@@ -7,6 +7,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/device.h>
+#include <linux/slab.h>
 
 #include <linux/fs.h>
 
@@ -14,8 +15,11 @@
 
 #include "hcsr_device.h"
 
-#define NUM_OF_DEVICES (10)                      /**< Number of devices supported by this driver*/
+#define CHAR_BUF_LEN (8)                        /**< Length of name buffer */
+#define NUM_OF_DEVICES (10)                     /**< Number of devices supported by this driver*/
 #define DEVICE_NAME "HCSR"                      /**< Sysfs class name */
+
+static const char *platform_name = "HCSR04";    /**< Constant platform name */
 
 static dev_t dev_num = 0;                       /**< Driver Major Number */
 static struct class *s_dev_class = NULL;        /**< Driver Class */
@@ -30,109 +34,7 @@ static void hcsr_device_release(struct device *dev) {
 };
 
 /**< hcsr devices description */
-static hcsr_device_t hcsr_devices[NUM_OF_DEVICES] = {
-        {
-                .name   = "HCSR_0",
-                .dev_no     = 0,
-                .plf_dev = {
-                        .name   = "HCSR04",
-                        .id = 0,
-                        .dev = {
-                                .release = hcsr_device_release
-                        }
-                },
-        }, {
-                .name   = "HCSR_1",
-                .dev_no     = 1,
-                .plf_dev = {
-                        .name   = "HCSR04",
-                        .id = 1,
-                        .dev = {
-                                .release = hcsr_device_release
-                        }
-                },
-        }, {
-                .name   = "HCSR_2",
-                .dev_no     = 1,
-                .plf_dev = {
-                        .name   = "HCSR04",
-                        .id = 1,
-                        .dev = {
-                                .release = hcsr_device_release
-                        }
-                },
-        }, {
-                .name   = "HCSR_3",
-                .dev_no     = 1,
-                .plf_dev = {
-                        .name   = "HCSR04",
-                        .id = 1,
-                        .dev = {
-                                .release = hcsr_device_release
-                        }
-                },
-        }, {
-                .name   = "HCSR_4",
-                .dev_no     = 1,
-                .plf_dev = {
-                        .name   = "HCSR04",
-                        .id = 1,
-                        .dev = {
-                                .release = hcsr_device_release
-                        }
-                },
-        }, {
-                .name   = "HCSR_5",
-                .dev_no     = 1,
-                .plf_dev = {
-                        .name   = "HCSR04",
-                        .id = 1,
-                        .dev = {
-                                .release = hcsr_device_release
-                        }
-                },
-        }, {
-                .name   = "HCSR_6",
-                .dev_no     = 1,
-                .plf_dev = {
-                        .name   = "HCSR04",
-                        .id = 1,
-                        .dev = {
-                                .release = hcsr_device_release
-                        }
-                },
-        }, {
-                .name   = "HCSR_7",
-                .dev_no     = 1,
-                .plf_dev = {
-                        .name   = "HCSR04",
-                        .id = 1,
-                        .dev = {
-                                .release = hcsr_device_release
-                        }
-                },
-        }, {
-                .name   = "HCSR_8",
-                .dev_no     = 1,
-                .plf_dev = {
-                        .name   = "HCSR04",
-                        .id = 1,
-                        .dev = {
-                                .release = hcsr_device_release
-                        }
-                },
-        }, {
-                .name   = "HCSR_9",
-                .dev_no     = 1,
-                .plf_dev = {
-                        .name   = "HCSR04",
-                        .id = 1,
-                        .dev = {
-                                .release = hcsr_device_release
-                        }
-                },
-        }
-};
+static hcsr_device_t *hcsr_devices = NULL;
 
 /** Device parameter n */
 static int n = 1;
@@ -149,12 +51,28 @@ static int hcsr_device_init(void)
         // Get a device number for the driver
         ret = alloc_chrdev_region(&dev_num, 0, n, DEVICE_NAME);
 
+        if (n > NUM_OF_DEVICES) {
+                printk(KERN_WARNING "Only supported %d devices.\n", NUM_OF_DEVICES);
+                n = NUM_OF_DEVICES;
+        }
+
+        hcsr_devices = kmalloc(n * sizeof(hcsr_device_t), GFP_KERNEL);
+        if (hcsr_devices == NULL) {
+                return -ENOMEM;
+        }
+
         s_dev_class = class_create(THIS_MODULE, DEVICE_NAME);
 
         // Register the device
         for (i = 0; i < n; i++) {
-                hcsr_devices[i].dev_class = s_dev_class;
-                hcsr_devices[i].dev_no = MKDEV(MAJOR(dev_num), i);
+                char *dev_name = kmalloc(CHAR_BUF_LEN, GFP_KERNEL);
+                snprintf(dev_name, CHAR_BUF_LEN, "%s_%d", DEVICE_NAME, i);
+                hcsr_devices[i].name                  = dev_name;
+                hcsr_devices[i].dev_class             = s_dev_class;
+                hcsr_devices[i].dev_no                = MKDEV(MAJOR(dev_num), i);
+                hcsr_devices[i].plf_dev.name          = platform_name;
+                hcsr_devices[i].plf_dev.id            = 0;
+                hcsr_devices[i].plf_dev.dev.release   = hcsr_device_release;
                 platform_device_register(&hcsr_devices[i].plf_dev);
         }
 
@@ -169,8 +87,10 @@ static void hcsr_device_exit(void)
         int i;
 
         for (i = 0; i < n; i++) {
+                kfree(hcsr_devices[i].name);
                 platform_device_unregister(&hcsr_devices[i].plf_dev);
         }
+        kfree(hcsr_devices);
 
         // Destroy driver_class
         class_destroy(s_dev_class);
