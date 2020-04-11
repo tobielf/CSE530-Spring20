09f69cd6bd18e21bfb1bbed69bb7754a2e5faf98
Xiangyu Guo <tobielf@gmail.com>
2020-04-10 21:50:16 -0700

    Update spi.sh
    Add a shield pin as SPI CS.

--- HW3/spi.sh
+++ HW3/spi.sh
@@ -1,3 +1,5 @@
+
+# Enable Shield pin 11 as SPI MOSI
 echo 24 > /sys/class/gpio/export
 echo 44 > /sys/class/gpio/export
 echo 72 > /sys/class/gpio/export
@@ -7,9 +9,14 @@ echo 0 > /sys/class/gpio/gpio24/value
 echo 1 > /sys/class/gpio/gpio44/value
 echo 0 > /sys/class/gpio/gpio72/value
 
+# Enable Shield pin 13 as SPI SCK
 echo 30 > /sys/class/gpio/export
 echo 46 > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio30/direction
 echo "out" > /sys/class/gpio/gpio46/direction
 echo 0 > /sys/class/gpio/gpio30/value
 echo 1 > /sys/class/gpio/gpio46/value
+
+# Use Shield pin 8 as SPI CS/LOAD
+echo 40 > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio40/direction
