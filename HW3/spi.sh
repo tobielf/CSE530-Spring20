ac50b70cc01f63b05de1751479f0b31152253ad5
Xiangyu Guo <tobielf@gmail.com>
2020-04-14 00:34:54 -0700

    Added more setups to the script.
    The testing script similar to the driver code. Set up GPIO multiplexing, turn off shutdown mode, and set scan limit to 8.

--- HW3/spi.sh
+++ HW3/spi.sh
@@ -20,3 +20,12 @@ echo 1 > /sys/class/gpio/gpio46/value
 # Use Shield pin 8 as SPI CS/LOAD
 echo 40 > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio40/direction
+
+# Turn off shutdown mode
+./spi_tester -p "\x01\x0c" -s 10 -b16; echo 1 > /sys/class/gpio/gpio40/value ; echo 0 > /sys/class/gpio/gpio40/value
+
+# Set scan limit to 8
+./spi_tester -p "\x08\x0b" -s 10 -b16; echo 1 > /sys/class/gpio/gpio40/value ; echo 0 > /sys/class/gpio/gpio40/value
+
+# Test Display
+./spi_tester -p "\x00\x0f" -s 10 -b16; echo 1 > /sys/class/gpio/gpio40/value ; echo 0 > /sys/class/gpio/gpio40/value
