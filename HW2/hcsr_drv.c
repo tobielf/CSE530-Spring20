47a58dd3aee9a762c9ff2237d3f05625c0863305
Xiangyu Guo <tobielf@gmail.com>
2020-04-15 02:15:34 -0700

    Added a callback notification.
    When the driver complete a measurement, it can notify someone. The caller can saved their context before calling the measurement and resume their execution from the saved context information.

--- HW2/hcsr_drv.c
+++ HW2/hcsr_drv.c
@@ -22,6 +22,8 @@
 #include "utils.h"
 
 #define HISTORY_SIZE    (5)                     /**< Sampling history size */
+#define DEFAULT_M       (4)                     /**< Default value for m */
+#define DEFAULT_DELTA   (200)                   /**< Default value for delta */
 
 /**
  * @brief: handling the echo pin interrupt.
@@ -111,12 +113,16 @@ int hcsr_init_one(struct hcsr_dev *devp) {
         atomic_set(&devp->settings.most_recent, 0);
 
         // Initialized default m and delta.
-        devp->settings.params.m = 4;
-        devp->settings.params.delta = 200;
+        devp->settings.params.m = DEFAULT_M;
+        devp->settings.params.delta = DEFAULT_DELTA;
 
         // Initialized default pins.
         devp->settings.pins.trigger_pin = -1;
         devp->settings.pins.echo_pin = -1;
+
+        // Initialized default call back.
+        devp->on_complete.context = 0;
+        devp->on_complete.notify = NULL;
 
         devp->irq_no = 0;
         devp->job_done = 1;
@@ -193,7 +199,7 @@ static int hcsr_sampling_thread(void *data) {
                         // Now we can safely start trigger m times.
                         while (m > 0) {
                                 gpio_set_value_cansleep(trigger_pin, 1);
-                                udelay(20);
+                                udelay(10);
                                 gpio_set_value_cansleep(trigger_pin, 0);
                                 msleep(delta);
                                 printk(KERN_INFO "sampling %d\n", m);
@@ -218,6 +224,10 @@ static int hcsr_sampling_thread(void *data) {
                         // After sampling, we need to add to the result_queue buff.
                         ring_buff_put(devp->result_queue, res);
                         atomic_set(&devp->settings.most_recent, res->measurement);
+                        // Do we need to notify someone?
+                        if (devp->on_complete.notify != NULL) {
+                                devp->on_complete.notify((unsigned long)res->measurement);
+                        }
                 } while (devp->settings.endless);
 
                 devp->job_done = 1;
