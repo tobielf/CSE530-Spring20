7f3778fd1b8422c95102aa8ab31a9e7e3902f404
Xiangyu Guo <tobielf@gmail.com>
2020-05-03 18:44:43 -0700

    Update sys_test.c
    Updated test cases.

--- HW4/sys_test.c
+++ HW4/sys_test.c
@@ -37,6 +37,25 @@
 */
 
 /**
+ * W calibrate_delay_is_known
+ * r sched_feat_fops
+ * R event_trigger_fops
+ * d tsc_irqwork
+ * D init_tss
+ * B dqstats
+ * b dquot_hash
+
+
+ *  ./tester probe calibrate_delay_is_known 0
+ *  ./tester probe sched_feat_fops 0
+ *  ./tester probe event_trigger_fops 0
+ *  ./tester probe tsc_irqwork 0
+ *  ./tester probe init_tss 0
+ *  ./tester probe dqstats 0
+ *  ./tester probe dquot_hash 0
+ */
+
+/**
  *
  */
 void *case9_thread_0(void *arg) {
@@ -72,11 +91,27 @@ void *case9_thread_1(void *arg) {
 /**
  *
  */
-void *case7_thread(void *arg) {
+void *dump_by_child(void *arg) {
     // try to dump it.
     int fd = *(int *)arg;
 
     write(fd, &fd, 0);
+    return NULL;
+}
+
+/**
+ *
+ */
+void *insert_by_child(void *arg) {
+    int ret;
+    int mode = *(int *)arg;
+    ret = insdump(TEST_SYMBOL1, mode);
+    if (ret < 0) {
+        printf("Failed! return: %d\n", ret);
+        return NULL;
+    }
+    sleep(10);
+    rmdump(ret);
     return NULL;
 }
 
@@ -85,8 +120,16 @@ void *case7_thread(void *arg) {
  */
 void *case5_thread(void *arg) {
     // dump it and exit.
+    int dump_id_1;
+    int dump_id_2;
     printf("Testing on dump and exit.\n");
-    insdump(TEST_SYMBOL2, 0);
+    dump_id_1 = insdump(TEST_SYMBOL2, 0);
+    dump_id_2 = insdump(TEST_SYMBOL1, 1);
+    if (dump_id_1 != dump_id_2) {
+        printf("Passed! dumpid1:%d dumpid2:%d\n", dump_id_1, dump_id_2);
+    } else {
+        printf("Failed! dumpid1:%d dumpid2:%d\n", dump_id_1, dump_id_2);
+    }
     printf("Please check dmesg for result.\n");
     return NULL;
 }
@@ -111,7 +154,7 @@ void *case4_thread(void *arg) {
 }
 
 void test_suite_six(void) {
-    // 10. dumpmode 2, inserted by parent process, dump by child process.
+    // 12. dumpmode 2, inserted by parent process, dump by child process.
     int ret = 0;
     int fd;
     pthread_t debug;
@@ -132,7 +175,7 @@ void test_suite_six(void) {
     write(fd, &ret, 1);
     printf("Please check dmesg for result.\n");
 
-    pthread_create(&debug, NULL, case7_thread, (void *)&fd);
+    pthread_create(&debug, NULL, dump_by_child, (void *)&fd);
     pthread_join(debug, NULL);
 
     close(fd);
@@ -140,7 +183,7 @@ void test_suite_six(void) {
 }
 
 void test_suite_five(void) {
-    // 9. dumpmode 1, inserted by child 1 process, dump by child 2 process.
+    // 11. dumpmode 1, inserted by child 1 process, dump by child 2 process.
     pthread_t debug[2];
 
     pthread_create(&debug[0], NULL, case9_thread_0, NULL);
@@ -150,29 +193,31 @@ void test_suite_five(void) {
     pthread_join(debug[1], NULL);
 }
 
+//[ToDo]
 void test_suite_four(void) {
-    // 8. dumpmode 1, inserted by parent process, dump by itself. [should print out nothing].
-    int ret = 0;
+    // 9. dumpmode 0, inserted by child process, dump by parent. [should print out nothing].
+    // 10. dumpmode 1, inserted by child process, dump by parent. [should print out nothing].
+    // 12. dumpmode 2, inserted by child process, dump by parent.
     int fd;
+    int mode;
+    pthread_t debug;
+    for (mode = 0; mode < 3; mode++) {
+        pthread_create(&debug, NULL, insert_by_child, (void *)&mode);
 
-    ret = insdump(TEST_SYMBOL1, 1);
-    if (ret < 0) {
-        printf("Insert dump failed, errno: %d\n", ret);
-    } else {
-        printf("Insert dump success, dumpid: %d\n", ret);
+        fd = open(TEST_DRV, O_RDWR);
+        if (fd < 0) {
+            printf("File not exist, insert the driver?\n");
+            return;
+        }
+
+        sleep(1);
+        write(fd, &mode, 1);
+        printf("Please check dmesg for result.\n");
+        close(fd);
+
+        pthread_join(debug, NULL);
+        printf("Finished inserted by child, dump by parent under mode:%d\n", mode);
     }
-
-    fd = open(TEST_DRV, O_RDWR);
-    if (fd < 0) {
-        printf("File not exist, insert the driver?\n");
-        return;
-    }
-
-    write(fd, &ret, 1);
-    printf("Please check dmesg for result.\n");
-
-    close(fd);
-    rmdump(ret);
 }
 
 void test_suite_three(void) {
@@ -198,7 +243,27 @@ void test_suite_three(void) {
     printf("Please check dmesg for result.\n");
 
     // 7. dumpmode 0, inserted by parent process, dump by child process.
-    pthread_create(&debug, NULL, case7_thread, (void *)&fd);
+    pthread_create(&debug, NULL, dump_by_child, (void *)&fd);
+    pthread_join(debug, NULL);
+
+    close(fd);
+    rmdump(ret);
+
+    // 8. dumpmode 1, inserted by parent process, dump by child. [should print out nothing].
+    ret = insdump(TEST_SYMBOL1, 1);
+    if (ret < 0) {
+        printf("Insert dump failed, errno: %d\n", ret);
+    } else {
+        printf("Insert dump success, dumpid: %d\n", ret);
+    }
+
+    fd = open(TEST_DRV, O_RDWR);
+    if (fd < 0) {
+        printf("File not exist, insert the driver?\n");
+        return;
+    }
+
+    pthread_create(&debug, NULL, dump_by_child, (void *)&fd);
     pthread_join(debug, NULL);
 
     close(fd);
