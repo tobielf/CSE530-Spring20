7aa7a00f697570fb9093546da0e84f167e14dfe2
Xiangyu Guo <tobielf@gmail.com>
2020-05-08 16:42:07 -0700

    Update run.sh
    Added comment to the preprocessing script.

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ cd submissions;
 for i in `ls`;
 do
     cd $i;
+
+    # Renaming the folders, unify the rest operations.
     # echo "####Renaminng for" $i;
     # if test -d "Part1"; then
     #     echo "Renaminng Part1";
@@ -22,12 +24,16 @@ do
     # fi;
 
     echo "####Checking for" $i;
-    # # echo -e "\n\n\n\n";
-    # echo -e "Files:\n";
-    # ls -la;
+    # Check for submission files & Makefile warnings.
+    # [ToDo] Creat a function for below, so we can reuse the code.
+    # echo -e "\n\n\n\n";
+    echo -e "Files:\n";
+    ls -la;
+
     echo -e "Makefile Wall flag?\n";
     cat Makefile | grep "Wall";
     
+    # Two parts assignment.
     # if test -d "part1"; then
     #     cd part1;
     #     echo -e "Files\n";
@@ -45,32 +51,37 @@ do
     #     cd ..;
     # fi;
 
-    # echo "####Compiling for" $i;
+    echo "####Compiling for" $i;
 
-    # dos2unix Makefile;
-    make "APP=tester";
+    dos2unix Makefile;
+    # Required student to put "APP" variable in their Makefile.
+    make "APP=tester -DGRADING";
 
-    # CMD="mkdir -p /home/root/students/${i}";
-    # ssh root@10.0.1.100 $CMD;
+    # Creat a remote directory using student's name.
+    CMD="mkdir -p /home/root/students/${i}";
+    ssh root@10.0.1.100 $CMD;
 
-    # # TESTER=$(cat Makefile | grep "APP =" | awk '{split($0,a," = "); print a[2];}');
-    # TESTER="tester";
-    # echo "Copying....";
-    # echo $TESTER;
-    # DIR="/home/root/students/${i}/";
-    # echo "to ${DIR}";
-    # scp $TESTER root@10.0.1.100:$DIR;
-    # echo "Done!";
+    # TESTER=$(cat Makefile | grep "APP =" | awk '{split($0,a," = "); print a[2];}');
+    TESTER="tester";
+    echo "Copying....";
+    echo $TESTER;
+    DIR="/home/root/students/${i}/";
+    echo "to ${DIR}";
+    scp $TESTER root@10.0.1.100:$DIR;
+    echo "Done!";
     make clean;
 
-    # #echo "" >> ../../patches.txt;
-    # DIR="/home/root/students/${i}/";
-    # for j in `ls *.patch`;
-    # do
-    #     mv ${j} new.patch
-    #     echo "${i}" >> ../../patches.txt;
-    # done;
+    # If it's kernel image assignment.
+    #echo "" >> ../../patches.txt;
+    DIR="/home/root/students/${i}/";
+    for j in `ls *.patch`;
+    do
+        mv ${j} new.patch
+        echo "${i}" >> ../../patches.txt;
+    done;
 
+    # Else kernel module assignment.
+    # [ToDo] Create a function, so we can resue the code.
     # if test -d "part1"; then
     #     cd part1;
     #     dos2unix Makefile;
@@ -119,6 +130,7 @@ do
     #     done;
     #     cd ..;
     # fi;
+
     echo -e "\n\n\n\n";
     cd ..;
 done
